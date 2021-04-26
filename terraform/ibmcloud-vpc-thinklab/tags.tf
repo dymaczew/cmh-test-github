7fdedc5f0b70d51084bb3c82c364257f3e28a63d
Wlodek Dymaczewski <dymaczewski@pl.ibm.com>
2021-04-26 20:55:15 +0200

    fixed variable case

--- terraform/ibmcloud-vpc-thinklab/tags.tf
+++ terraform/ibmcloud-vpc-thinklab/tags.tf
@@ -1,7 +1,7 @@
 locals {
   createdDate = formatdate( "DD MMM YYYY hh:mm ZZZ", timestamp() )
   validThrough = timeadd(local.createdDate, "2h")
-  lifecycle_tags = [ "createDate:${local.CreatedDate}", "validThrough:${local.validThrough}"]
+  lifecycle_tags = [ "createDate:${local.createdDate}", "validThrough:${local.validThrough}"]
 
   # Usage - tags = merge(local.common_tags) in resources.
 }
