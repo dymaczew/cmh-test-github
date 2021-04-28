efd296c1dadbf73408ea021dc959329e17e394e6
Wlodek Dymaczewski <dymaczewski@pl.ibm.com>
2021-04-28 11:07:59 +0200

    added random resource id

--- terraform/ibmcloud-vpc-thinklab/main.tf
+++ terraform/ibmcloud-vpc-thinklab/main.tf
@@ -1,27 +1,31 @@
 provider "ibm" {
-  region = "${var.region}"
+  region = ${var.region}
+}
+
+resource "random_id" "default" {
+  byte_length = "4"
 }
 
 resource "ibm_is_vpc" "test_vpc" {
-  name    = join("-", [ "vpc", var.username ])
+  name    = "${var.prefix}-${random_id.default.hex}-vpc"
   tags    = "${concat(var.tags, module.camtags.tagslist)}"
 }
 
 resource "ibm_is_subnet" "test_subnet" {
-  name    = join("-", [ "subnet", var.username ])
+  name    = "${var.prefix}-${random_id.default.hex}-subnet"
   vpc             = "${ibm_is_vpc.test_vpc.id}"
   zone            = "${var.zone}"
   ipv4_cidr_block = "10.240.0.0/18"
 }
 
 resource "ibm_is_ssh_key" "test_sshkey" {
-  name    = join("-", [ "sshkey", var.username ])
+  name    = "${var.prefix}-${random_id.default.hex}-sshkey"
   public_key = "${var.public_ssh_key}"
 }
 
 ## Web server VSI
 resource "ibm_is_instance" "web-server" {
-  name    = join("-", ["web-server-vsi", var.username ])
+  name    = "${var.prefix}-${random_id.default.hex}-web-server-vsi"
   image   = "${var.image_id}"
   profile = "${var.profile}"
 
@@ -43,13 +47,13 @@ resource "ibm_is_instance" "web-server" {
 
 ## Attach floating IP address to web server VSI
 resource "ibm_is_floating_ip" "test_floatingip" {
-  name   = "test-fip"
+  name   = "${var.prefix}-${random_id.default.hex}-fip"
   target = "${ibm_is_instance.web-server.primary_network_interface.0.id}"
 }
 
 ## DB server VSI with additional data volume
 resource "ibm_is_instance" "db-server" {
-  name    = join("-", ["db-server-vsi", var.username ])
+  name    = "${var.prefix}-${random_id.default.hex}-db-server-vsi"
   image   = "${var.image_id}"
   profile = "${var.profile}"
 
@@ -71,12 +75,18 @@ resource "ibm_is_instance" "db-server" {
 }
 
 resource "ibm_is_volume" "test-volume" {
-  name     = join("-", ["volume", var.username ])
+  name     = "${var.prefix}-${random_id.default.hex}-volume"
   profile  = "custom"
   zone     = "${var.zone}"
   iops     = 1000
   capacity = 20
-  tags    = "${concat(var.tags, module.camtags.tagslist)}"
+  tags    = "${concat(var.tags, module.camtags.tagslist, local.lifecycle_tags)}"
+
+  lifecycle {
+  ignore_changes = [
+    tags
+  ]
+  }
 }
 
 module "camtags" {
