44b3aeb72040d4c6e3114207df6649753a0c49a0
Wlodek Dymaczewski <dymaczewski@pl.ibm.com>
2021-06-15 13:33:20 +0200

    fixed quotes

--- terraform/ibmcloud-vpc-infralab/main.tf
+++ terraform/ibmcloud-vpc-infralab/main.tf
@@ -9,7 +9,7 @@ resource "random_id" "default" {
 resource "ibm_is_vpc" "test_vpc" {
   name    = "${var.prefix}-${random_id.default.hex}-vpc"
   tags    = "${concat(var.tags, module.camtags.tagslist)}"
-  resource_group = "${var.resource_group} 
+  resource_group = "${var.resource_group}"
 }
 
 resource "ibm_is_subnet" "test_subnet" {
@@ -17,13 +17,13 @@ resource "ibm_is_subnet" "test_subnet" {
   vpc             = "${ibm_is_vpc.test_vpc.id}"
   zone            = "${var.zone}"
   ipv4_cidr_block = "10.240.0.0/24"
-  resource_group = "${var.resource_group} 
+  resource_group = "${var.resource_group}"
 }
 
 resource "ibm_is_ssh_key" "test_sshkey" {
   name    = "${var.prefix}-${random_id.default.hex}-sshkey"
   public_key = "${var.public_ssh_key}"
-  resource_group = "${var.resource_group} 
+  resource_group = "${var.resource_group}" 
 }
 
 ## Web server VSI
@@ -32,7 +32,7 @@ resource "ibm_is_instance" "web-server" {
   image   = "${var.image_id}"
   profile = "${var.profile}"
   user_data = file("userdata.tpl")
-  resource_group = "${var.resource_group} 
+  resource_group = "${var.resource_group}" 
 
   primary_network_interface {
     subnet = "${ibm_is_subnet.test_subnet.id}"
@@ -55,7 +55,7 @@ resource "ibm_is_instance" "web-server" {
 resource "ibm_is_floating_ip" "test_floatingip" {
   name   = "${var.prefix}-${random_id.default.hex}-fip"
   target = "${ibm_is_instance.web-server.primary_network_interface.0.id}"
-  resource_group = "${var.resource_group} 
+  resource_group = "${var.resource_group}" 
 }
 
 ## DB server VSI with additional data volume
@@ -63,7 +63,7 @@ resource "ibm_is_instance" "db-server" {
   name    = "${var.prefix}-${random_id.default.hex}-db-server-vsi"
   image   = "${var.image_id}"
   profile = "${var.profile}"
-  resource_group = "${var.resource_group} 
+  resource_group = "${var.resource_group}" 
 
   primary_network_interface {
     subnet = "${ibm_is_subnet.test_subnet.id}"
@@ -89,7 +89,7 @@ resource "ibm_is_volume" "test-volume" {
   iops     = 1000
   capacity = 20
   tags    = "${concat(var.tags, module.camtags.tagslist, local.lifecycle_tags)}"
-  resource_group = "${var.resource_group} 
+  resource_group = "${var.resource_group}" 
 
   lifecycle {
   ignore_changes = [
